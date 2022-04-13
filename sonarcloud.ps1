f30cc8175d1e13b3bebe6fb24c7b43256448d8e9
Jario <rochajario@gmail.com>
2022-04-13 18:18:21 -0300

    test

--- sonarcloud.ps1
+++ sonarcloud.ps1
@@ -26,12 +26,9 @@ Write-Host "branch is $branch"
 dotnet tool restore
 dotnet tool run dotnet-sonarscanner begin /k:"rochajario_B3-Desafio-Cdb" /o:"rochajario" /d:sonar.login="$SONAR_TOKEN" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cs.vstest.reportsPaths=TestResults/*.trx /d:sonar.cs.opencover.reportsPaths=TestResults/*/coverage.opencover.xml /d:sonar.coverage.exclusions="**Test*.cs" /d:sonar.branch.name="$branch"
 
-#Adding Test Coverage to Sonar
-mkdir "$runningDirectory/.sonarqube"
-
 dotnet restore ./
 dotnet build ./ --configuration release
-dotnet test "./Test/Domain.Test.csproj" --collect:"XPlat Code Coverage" --results-directory TestResults/ --logger "trx;LogFileName=unittests.trx" --no-build --no-restore --configuration release -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format=opencover
+dotnet test "./Test/Domain.Test.csproj" --collect:"XPlat Code Coverage" --logger "trx;LogFileName=unittests.trx" --no-build --no-restore --configuration release -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format=opencover
          
 
 dotnet tool run dotnet-sonarscanner end /d:sonar.login="$sonarSecret"
