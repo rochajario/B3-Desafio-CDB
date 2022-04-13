b1b9d5548d56aab34094baba06e015c23faab5ec
Jario <rochajario@gmail.com>
2022-04-13 16:31:48 -0300

    Adjusting Environment Variable

--- sonarcloud.ps1
+++ sonarcloud.ps1
@@ -23,7 +23,7 @@ $branch = git branch --show-current
 Write-Host "branch is $branch"
 
 dotnet tool restore
-dotnet tool run dotnet-sonarscanner begin /k:"rochajario_B3-Desafio-Cdb" /o:"rochajario" /d:sonar.login="${{ secrets.SONAR_TOKEN }}" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cs.vstest.reportsPaths=TestResults/*.trx /d:sonar.cs.opencover.reportsPaths=TestResults/*/coverage.opencover.xml /d:sonar.coverage.exclusions="**Test*.cs" /d:sonar.branch.name="$branch"
+dotnet tool run dotnet-sonarscanner begin /k:"rochajario_B3-Desafio-Cdb" /o:"rochajario" /d:sonar.login="$SONAR_TOKEN" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cs.vstest.reportsPaths=TestResults/*.trx /d:sonar.cs.opencover.reportsPaths=TestResults/*/coverage.opencover.xml /d:sonar.coverage.exclusions="**Test*.cs" /d:sonar.branch.name="$branch"
 
 dotnet restore src
 dotnet build src --configuration release
