fc191954a65397bbd159ae4f4cd64d9f05edc1ac
Jario <rochajario@gmail.com>
2022-04-13 16:40:38 -0300

    Creating test folder to ci/cd

--- sonarcloud.ps1
+++ sonarcloud.ps1
@@ -9,6 +9,7 @@ Import-Module BuildUtils
 $runningDirectory = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 
 $testOutputDir = "$runningDirectory/TestResults"
+mkdir $testOutputDir
 
 if (Test-Path $testOutputDir) 
 {
